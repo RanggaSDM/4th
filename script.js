28d74acce3baa6a581004c996f833118d10c24c4
RanggaSDM <rangga.sidemen@gmail.com>
2024-07-24 03:19:52 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    document.getElementById('specialMessage').style.display = 'block';
+}
+
+// Masukkan jumlah tahun kalian bersama di sini
+let years = 4; // Contoh: 2 tahun
+document.getElementById('years').innerText = years;
